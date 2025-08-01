0957ab43ed69dd085356b08fc1045cac05a04844
SAP-Code-World <214628155+SAP-code-world@users.noreply.github.com>
2025-08-01 23:25:03 +0000

    Release v1.6.0: Added Logging feature

--- types/lib/log/index.d.ts
+++ types/lib/log/index.d.ts
@@ -1,7 +1,8 @@
 export function log(sServiceName?: any): {
-    trace: (oEvent: any, oRequest: any, ...args: any[]) => void;
-    debug: (oEvent: any, oRequest: any, ...args: any[]) => void;
-    info: (oEvent: any, oRequest: any, ...args: any[]) => void;
-    warn: (oEvent: any, oRequest: any, ...args: any[]) => void;
-    error: (oEvent: any, oRequest: any, ...args: any[]) => void;
+    trace: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
+    debug: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
+    info: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
+    warn: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
+    error: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
+    fatal: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
 };
