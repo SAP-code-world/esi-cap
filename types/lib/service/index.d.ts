155c09e5a37f89056d4267b1ce36014f1b7a37e9
SAP-code-world <sap.consultant.freelancer@gmail.com>
2026-04-20 21:48:59 +0000

    chore(release): v1.7.36 artifacts

--- types/lib/service/index.d.ts
+++ types/lib/service/index.d.ts
@@ -1,33 +1,110 @@
-export namespace service {
-    namespace events {
-        let PreBefore: string;
-        let PostBefore: string;
-        let PreOn: string;
-        let PreMeshUp: string;
-        let PostOn: string;
-        let PreAfter: string;
-        let PostAfter: string;
-        let None: string;
-    }
-    function enrichUserContext(oRequest: object): Promise<void>;
-    function sanitizeData(oData: any, oFieldsArray: any, includeID?: boolean): any;
-    function get(oRequest: any): any;
-    function getEntityColumns(oService: object, oRequest: object): string[];
-    function sendQuery(oService: object, oRequest: object, oQuery?: object): Promise<any>;
-    function sendRequest(oService: object, oRequest: object): Promise<any>;
-    function executeRequest(oService: object, oRequest: object): Promise<any>;
-    namespace iflow {
-        function call(oRequest: object, oJsonData: object, onError?: () => Promise<void>): Promise<void>;
-    }
-    function handleReadByID(oService: any, oRequest: any, oID: any): Promise<any>;
-    function handleDeleteByID(oService: any, oRequest: any, oID: any): Promise<any>;
-    function handleEvent(oService: object, oRequest: object, fEventHandler?: Function): Promise<any>;
-    function handleCreate(oService: object, oRequest: object, fPreInsert?: Function): Promise<any>;
-    function handleInsert(oService: object, oRequest: object, fPreInsert?: Function): Promise<any>;
-    function handleUpdate(oService: object, oRequest: object, fPreUpdate: Function, oWhere: any, isOldDataRequired?: boolean): Promise<any>;
-    function handleCount(oData: object, oRequest: object): Promise<void>;
-    function handlePostOn(oService: object, oRequest: object): Promise<any[]>;
-    function handleHandler(oService: any, oRequest: any): Promise<any>;
-    function handleRead(oService: object, oRequest: object, fNextHandler: Function): Promise<any>;
+/**
+ * service module
+ * @class
+ * @public
+ */
+export class service {
+    /** All Service Events
+     * @public
+     * @static
+     * @enum {string} */
+    public static events: Readonly<{
+        PreBefore: "PreBefore";
+        PostBefore: "PostBefore";
+        PreOn: "PreOn";
+        PreMeshUp: "PreMeshUp";
+        PostOn: "PostOn";
+        PreAfter: "PreAfter";
+        PostAfter: "PostAfter";
+        None: "";
+    }>;
+    /** This function enriches the user context with configured user function
+     * @param {object} oRequest - Request Object
+     */
+    static enrichUserContext(oRequest: object): Promise<void>;
+    static sanitizeData(oData: any, oFieldsArray: any, includeID?: boolean): any;
+    static get(oRequest: any): any;
+    /**
+     * @param {object} oService - Service object for which request to be executed
+     * @param {object} oRequest - Request Object
+     * @returns {string[]}
+     */
+    static getEntityColumns(oService: object, oRequest: object): string[];
+    /**
+     * @param {object} oService - Service object for which request to be executed
+     * @param {object} oRequest - Request Object
+     * @param {object} oQuery - Query to be send
+     */
+    static sendQuery(oService: object, oRequest: object, oQuery?: object): Promise<any>;
+    /**
+     * @param {object} oService - Service object for which request to be executed
+     * @param {object} oRequest - Request Object
+     */
+    static sendRequest(oService: object, oRequest: object): Promise<any>;
+    /**
+     * @param {object} oService - Service object for which request to be executed
+     * @param {object} oRequest - Request Object
+     */
+    static executeRequest(oService: object, oRequest: object): Promise<any>;
+    /**
+     * @param {object} oRequest - Request Object
+     * @param {object} oJsonData - JSON Data
+     */
+    static executeIflow(oRequest: object, oJsonData: object, onError?: () => Promise<void>): Promise<void>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     */
+    static handleReadByID(oService: object, oRequest: object, oID: any): Promise<any>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     */
+    static handleDeleteByID(oService: object, oRequest: object, oID: any): Promise<any>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     * @param {function} fEventHandler - function to be called before Event for data handling
+     */
+    static handleEvent(oService: object, oRequest: object, fEventHandler?: Function): Promise<any>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     * @param {function} fPreInsert - function to be called before insert
+     */
+    static handleCreate(oService: object, oRequest: object, fPreInsert?: Function): Promise<any>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     * @param {function} fPreInsert - function to be called before insert
+     */
+    static handleInsert(oService: object, oRequest: object, fPreInsert?: Function): Promise<any>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     * @param {function} fPreUpdate - function to be called before Update
+     */
+    static handleUpdate(oService: object, oRequest: object, fPreUpdate: Function, oWhere: any, isOldDataRequired?: boolean): Promise<any>;
+    /**
+     * @param {object} oData - Data Result
+     * @param {object} oRequest - Request Object
+     */
+    static handleCount(oData: object, oRequest: object): Promise<void>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     */
+    static handlePostOn(oService: object, oRequest: object): Promise<any[]>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     */
+    static handleHandler(oService: object, oRequest: object): Promise<any>;
+    /**
+     * @param {object} oService - Service Object
+     * @param {object} oRequest - Request Object
+     * @param {function} fNextHandler - Alternate handler method
+     */
+    static handleRead(oService: object, oRequest: object, fNextHandler: Function): Promise<any>;
 }
 //# sourceMappingURL=index.d.ts.map
