82cf489cf0aa2247e26179fefd25b745390c48e6
SAP-code-world <sap.consultant.freelancer@gmail.com>
2026-04-20 23:31:08 +0000

    chore(release): v1.7.39 artifacts

--- types/lib/impl/index.d.ts
+++ types/lib/impl/index.d.ts
@@ -1,56 +1,51 @@
-/**
- * impl module
- */
-export type Service = typeof import("../service").service;
-/**
- * impl module
- */
-export type ServiceEvents = Service["events"];
-/**
- * impl module
- */
-export type ServiceEventsKey = keyof ServiceEvents;
 export type ServiceEventInterceptor = (oService: object) => Promise<void>;
+export type Service = typeof import("../service").service;
+export type ServiceEvents = Service["events"];
+export type ServiceEventsKey = keyof ServiceEvents;
+export type ServieEventsHandler = Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>;
 /**
  * impl module
  * @class
  * @public
- * @typedef {typeof import('../service').service} Service
- * @typedef {Service['events']} ServiceEvents
- * @typedef {keyof ServiceEvents} ServiceEventsKey
  */
 /**
  * @callback ServiceEventInterceptor
  * @param {object} oService - Service object for which request to be executed
  * @returns {Promise<void>}
  */
+/**
+ * @typedef {typeof import('../service').service} Service
+ * @typedef {Service['events']} ServiceEvents
+ * @typedef {keyof ServiceEvents} ServiceEventsKey
+ * @typedef {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} ServieEventsHandler
+ */
 export class impl {
     /**
      * @param {object} oService - Service object for which request to be executed
-     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oEvent
+     * @param {ServieEventsHandler} oEvent
      * @returns {Promise<void>}
      */
-    static DBService(oService: object, oEvent: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>): Promise<void>;
+    static DBService(oService: object, oEvent: ServieEventsHandler): Promise<void>;
     /**
      * @param {object} oService - Service object for which request to be executed
      * @param {string} sServiceName - Service Name
-     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oEvent
+     * @param {ServieEventsHandler} oEvent
      * @param {string|any[]} oLocalEntities
-     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oRemoteEvent
+     * @param {ServieEventsHandler} oRemoteEvent
      * @param {string|any[]} [oRemoteEntities]
      * @param {any[]} [oRemoteHandlers]
      * @param {boolean} bRefreshUserContext
      * @returns {Promise<void>}
      */
-    static LocalService(oService: object, sServiceName: string, oEvent?: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>, oLocalEntities?: string | any[], oRemoteEvent?: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>, oRemoteEntities?: string | any[], oRemoteHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
+    static LocalService(oService: object, sServiceName: string, oEvent?: ServieEventsHandler, oLocalEntities?: string | any[], oRemoteEvent?: ServieEventsHandler, oRemoteEntities?: string | any[], oRemoteHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
     /**
      * @param {object} oService - Service object for which request to be executed
-     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oEvent
+     * @param {ServieEventsHandler} oEvent
      * @param {string|any[]} oEntities
      * @param {any[]} [oHandlers]
      * @param {boolean} bRefreshUserContext
      * @returns {Promise<void>}
      */
-    static RemoteService(oService: object, oEvent?: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>, oEntities?: string | any[], oHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
+    static RemoteService(oService: object, oEvent?: ServieEventsHandler, oEntities?: string | any[], oHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
 }
 //# sourceMappingURL=index.d.ts.map
--- types/lib/log/index.d.ts
+++ types/lib/log/index.d.ts
@@ -1,9 +1,18 @@
-export function log(sServiceName?: any): {
-    trace: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
-    debug: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
-    info: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
-    warn: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
-    error: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
-    fatal: (oEvent: any, oRequest: any, ...oArgs: any[]) => void;
-};
+declare namespace _exports {
+    export { Service, ServiceEvents, ServiceEventsValue };
+}
+declare namespace _exports {
+    function log(sServiceName?: string | undefined): {
+        trace: (oEvent: ServiceEventsValue, oRequest: object, ...oArgs: any[]) => void;
+        debug: (oEvent: ServiceEventsValue, oRequest: object, ...oArgs: any[]) => void;
+        info: (oEvent: ServiceEventsValue, oRequest: object, ...oArgs: any[]) => void;
+        warn: (oEvent: ServiceEventsValue, oRequest: object, ...oArgs: any[]) => void;
+        error: (oEvent: ServiceEventsValue, oRequest: object, ...oArgs: any[]) => void;
+        fatal: (oEvent: ServiceEventsValue, oRequest: object, ...oArgs: any[]) => void;
+    };
+}
+export = _exports;
+type Service = typeof import("../service").service;
+type ServiceEvents = Service["events"];
+type ServiceEventsValue = ServiceEvents[keyof ServiceEvents];
 //# sourceMappingURL=index.d.ts.map
