335cbb084f3d98ea092e54dc3430c49f4bd81098
SAP-code-world <sap.consultant.freelancer@gmail.com>
2026-04-20 23:04:04 +0000

    chore(release): v1.7.38 artifacts

--- types/lib/impl/index.d.ts
+++ types/lib/impl/index.d.ts
@@ -1,13 +1,23 @@
 /**
  * impl module
  */
-export type ServiceEvents = import("../service").service.events;
+export type Service = typeof import("../service").service;
+/**
+ * impl module
+ */
+export type ServiceEvents = Service["events"];
+/**
+ * impl module
+ */
+export type ServiceEventsKey = keyof ServiceEvents;
 export type ServiceEventInterceptor = (oService: object) => Promise<void>;
 /**
  * impl module
  * @class
  * @public
- * @typedef {import('../service').service.events} ServiceEvents
+ * @typedef {typeof import('../service').service} Service
+ * @typedef {Service['events']} ServiceEvents
+ * @typedef {keyof ServiceEvents} ServiceEventsKey
  */
 /**
  * @callback ServiceEventInterceptor
@@ -17,30 +27,30 @@ export type ServiceEventInterceptor = (oService: object) => Promise<void>;
 export class impl {
     /**
      * @param {object} oService - Service object for which request to be executed
-     * @param {Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>} oEvent
+     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oEvent
      * @returns {Promise<void>}
      */
-    static DBService(oService: object, oEvent: Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>): Promise<void>;
+    static DBService(oService: object, oEvent: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>): Promise<void>;
     /**
      * @param {object} oService - Service object for which request to be executed
      * @param {string} sServiceName - Service Name
-     * @param {Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>} oEvent
+     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oEvent
      * @param {string|any[]} oLocalEntities
-     * @param {Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>} oRemoteEvent
+     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oRemoteEvent
      * @param {string|any[]} [oRemoteEntities]
      * @param {any[]} [oRemoteHandlers]
      * @param {boolean} bRefreshUserContext
      * @returns {Promise<void>}
      */
-    static LocalService(oService: object, sServiceName: string, oEvent?: Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>, oLocalEntities?: string | any[], oRemoteEvent?: Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>, oRemoteEntities?: string | any[], oRemoteHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
+    static LocalService(oService: object, sServiceName: string, oEvent?: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>, oLocalEntities?: string | any[], oRemoteEvent?: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>, oRemoteEntities?: string | any[], oRemoteHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
     /**
      * @param {object} oService - Service object for which request to be executed
-     * @param {Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>} oEvent
+     * @param {Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>} oEvent
      * @param {string|any[]} oEntities
      * @param {any[]} [oHandlers]
      * @param {boolean} bRefreshUserContext
      * @returns {Promise<void>}
      */
-    static RemoteService(oService: object, oEvent?: Partial<Record<keyof ServiceEvents, ServiceEventInterceptor>>, oEntities?: string | any[], oHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
+    static RemoteService(oService: object, oEvent?: Partial<Record<ServiceEventsKey, ServiceEventInterceptor>>, oEntities?: string | any[], oHandlers?: any[], bRefreshUserContext?: boolean): Promise<void>;
 }
 //# sourceMappingURL=index.d.ts.map
